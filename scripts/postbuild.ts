ae58b762252e2856ed459113526770d89a592a41
xiaochenchen <xiaochen.chen@igg.com>
2022-12-14 09:13:03 +0800

    chore

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,17 +1,18 @@
-import { basename, resolve } from 'path'
+import { basename, dirname, resolve } from 'path'
 import { promises as fs } from 'fs'
+import { fileURLToPath } from 'url'
 import fg from 'fast-glob'
+import chalk from 'chalk'
 
 async function run() {
   // fix cjs exports
-  const files = await fg('*.js', {
-    ignore: ['index.js', 'chunk-*'],
+  const files = await fg('*.cjs', {
+    ignore: ['chunk-*'],
     absolute: true,
-    cwd: resolve(__dirname, '../dist'),
+    cwd: resolve(dirname(fileURLToPath(import.meta.url)), '../dist'),
   })
   for (const file of files) {
-    // eslint-disable-next-line no-console
-    console.log('[postbuild]', basename(file))
+    console.log(chalk.cyan.inverse(' POST '), `Fix ${basename(file)}`)
     let code = await fs.readFile(file, 'utf8')
     code = code.replace('exports.default =', 'module.exports =')
     code += 'exports.default = module.exports;'
